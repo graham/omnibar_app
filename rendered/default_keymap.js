2cc602466a6bdbba40eba97343ba67ec2fac9197
Graham Abbott <graham.abbott@gmail.com>
2015-10-18 16:42:04 -0700

    adding rendered javascript to the gh-pages version.

---
+++ rendered/default_keymap.js
@@ -0,0 +1,62 @@
+// This keymap is a key-combo to event mapping.
+
+'use strict';
+
+var global_active_keymap = {
+    'control-c': 'command:cancel',
+    'control-s': 'command:search',
+    'control-d': 'command:do'
+};
+
+var global_passive_keymap = {
+    'control-x b': 'command:before',
+    'control-x a': 'command:after',
+    'control-x g': 'command:go',
+
+    'g': 'command:go',
+    'f': 'command:filter'
+};
+
+// These events get passed to the top view controller and only
+// that view controller.
+
+var mvc_passive_keymap = {
+    'shift-,': 'control:move_top',
+    'shift-k': 'control:move_up_more',
+    'up': 'control:move_up',
+    'k': 'control:move_up',
+
+    'shift-.': 'control:move_bottom',
+    'shift-j': 'control:move_down_more',
+    'down': 'control:move_down',
+    'j': 'control:move_down',
+    'h': 'control:help',
+
+    'space': 'control:select',
+    'x': 'control:select',
+
+    'z': 'control:deselect_all',
+    'a': 'control:select_all',
+
+    'y': 'command_multi:archive',
+    'u': 'command_multi:return',
+    'p': 'command_multi:pull',
+
+    'l': 'command_multi:label',
+    'o': 'command_multi:open',
+    'i': 'command_multi:info',
+
+    '-': 'command_multi:subtract',
+    '=': 'command_multi:add',
+    '[': 'command_multi:left',
+    ']': 'command_multi:right',
+
+    'control-n': 'command_single:next',
+    'control-p': 'command_single:prev',
+
+    'e': 'command_single:edit',
+    'r': 'command_single:reply',
+    'enter': 'command_single:open'
+};
+
+var mvc_active_keymap = {};
